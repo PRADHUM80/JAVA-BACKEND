c4c2aa2c8094d6ff8e226e6deb0e58661e534670
Pradhum80 <kumarroney154@gmail.com>
2026-09-02 11:58:59 +0530

    Define Java : Features

---
+++ Day-0/Define.java
@@ -0,0 +1,15 @@
+
+class Define
+{
+    public static void main(String[] args)
+    {
+        System.out.println("JAVA:- Java is a High-Level, Object-Orineted, Platform-Independent Programming language used Devleope Different types of Software and Application.");
+        System.out.println("Main Features :- ");
+        System.out.println("1.Object-Oriented :-  Programs are built using classes and objects.");
+        System.out.println("2.Platform Independent :- Java follows Write Once, Run Anywhere (WORA).");
+        System.out.println("3.Secure :- Provides features for safer application execution.");
+        System.out.println("4.Robust :- Has exception handling and automatic memory management.");
+        System.out.println("5.Portable :- Java programs can run on different operating systems.");
+        System.out.println("6.Multithreaded :- Can perform multiple tasks at the same time.");
+    }
+}
