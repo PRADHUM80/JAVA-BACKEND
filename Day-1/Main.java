4b75e7a327d6a6db03311744c15dcfd69a600d88
Pradhum80 <kumarroney154@gmail.com>
2026-09-02 10:00:59 +0530

    Add Day-0 and Day-1 Java code

---
+++ Day-1/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+
+public class Session
+{
+    public static void main(String[] args) {
+        System.out.println("Session started.");
+    }
+}
